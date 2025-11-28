3c331e0b618c8df25f2c1fab1876c6a62dfc5cb6
Alfie213 <arturbogoratov@gmail.com>
2025-11-28 16:57:41 +0400

    Update InventoryView.cs

--- Assets/src/Scripts/InventorySystem/InventoryView.cs
+++ Assets/src/Scripts/InventorySystem/InventoryView.cs
@@ -4,9 +4,12 @@ using UnityEngine.UI;
 
 public class InventoryView : MonoBehaviour
 {
+    [Header("UI Elements")]
     [SerializeField] private GameObject contentRoot;
     [SerializeField] private Button ticketButton;
     [SerializeField] private TextMeshProUGUI countText;
+    
+    [Header("Animation")]
     [SerializeField] private PulseAnimation pulseAnimation;
 
     private void Start()
@@ -20,8 +23,17 @@ public class InventoryView : MonoBehaviour
 
     private void UpdateVisuals(int count)
     {
-        if (count > 0 && !contentRoot.activeSelf) contentRoot.SetActive(true);
-        countText.text = count.ToString();
-        if (pulseAnimation != null) pulseAnimation.Play();
+        bool shouldBeVisible = count > 0;
+
+        if (contentRoot.activeSelf != shouldBeVisible)
+        {
+            contentRoot.SetActive(shouldBeVisible);
+        }
+
+        if (shouldBeVisible)
+        {
+            countText.text = count.ToString();
+            if (pulseAnimation != null) pulseAnimation.Play();
+        }
     }
 }
